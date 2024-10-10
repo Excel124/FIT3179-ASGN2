1f95a28902b54abcdb9902feeaba2804de5db749
kleo0027 <58525795+Excel124@users.noreply.github.com>
2024-10-11 03:02:22 +0800

    Create vega_lite_vis.js

---
+++ vega_lite_vis.js
@@ -0,0 +1,25 @@
+    var vg_1 = "malaysia_map.json";
+    var vg_2 = "tourist_arrivals.json";
+    var vg_3 = "top_source_countries.json";
+    var vg_4 = "tourism_revenue.json";
+    var vg_5 = "popular_activities.json";
+
+    vegaEmbed("#vis1", vg_1).then(function(result) {
+      // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+    vegaEmbed("#vis2", vg_2).then(function(result) {
+      // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+    vegaEmbed("#vis3", vg_3).then(function(result) {
+      // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+    vegaEmbed("#vis4", vg_4).then(function(result) {
+      // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
+
+    vegaEmbed("#vis5", vg_5).then(function(result) {
+      // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+    }).catch(console.error);
